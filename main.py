659830d60b41e4fc8892513d8d072aa7e63d2326
Daniel Pires Quirino <danielpires6@hotmail.com>
2019-12-15 11:26:11 -0300

    Adicionado tratamento de colisão entre a barra e a bola

--- main.py
+++ main.py
@@ -33,8 +33,8 @@ ball.shape("square")
 ball.color("white")
 ball.penup()
 ball.goto(0, 0)
-ball.dx = 0.1
-ball.dy = 0.1
+ball.dx = 0.2
+ball.dy = 0.2
 
 # Function
 def paddle_a_up():
@@ -87,4 +87,11 @@ while True:
 
     if ball.xcor() < -390:
         ball.goto(0, 0)
+        ball.dx *= -1
+
+    # Paddle and ball collisions
+    if ball.xcor() < -340 and ball.ycor() < paddle_a.ycor() + 50 and ball.ycor() > paddle_a.ycor() - 50:
+        ball.dx *= -1
+
+    if ball.xcor() > 340 and ball.ycor() < paddle_b.ycor() + 50 and ball.ycor() > paddle_b.ycor() - 50:
         ball.dx *= -1
